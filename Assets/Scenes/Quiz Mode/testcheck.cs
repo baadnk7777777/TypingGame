c3204c2087dfbe5fabd68f9ef86c8eca87bc1caa
Bukore <s6330613010@phuket.psu.ac.th>
2021-02-09 23:01:02 +0700

    Make Quiz

---
+++ Assets/Scenes/Quiz Mode/testcheck.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class testcheck : MonoBehaviour
+{
+    //busines test
+    public void OnButtonTCB1Click()
+    {
+        Application.LoadLevel(23);
+    }
+    public void OnButtonTCB2Click()
+    {
+        Application.LoadLevel(29);
+    }
+    public void OnButtonTCB3Click()
+    {
+        Application.LoadLevel(35);
+    }
+    public void OnButtonTCB4Click()
+    {
+        Application.LoadLevel(41);
+    }
+    //office test
+    public void OnButtonTCO1Click()
+    {
+        Application.LoadLevel(47);
+    }
+    public void OnButtonTCO2Click()
+    {
+        Application.LoadLevel(53);
+    }
+    public void OnButtonTCO3Click()
+    {
+        Application.LoadLevel(59);
+    }
+    public void OnButtonTCO4Click()
+    {
+        Application.LoadLevel(65);
+    }
+    //purchasing test
+    public void OnButtonTCP1Click()
+    {
+        Application.LoadLevel(71);
+    }
+    public void OnButtonTCP2Click()
+    {
+        Application.LoadLevel(77);
+    }
+    public void OnButtonTCP3Click()
+    {
+        Application.LoadLevel(83);
+    }
+    public void OnButtonTCP4Click()
+    {
+        Application.LoadLevel(89);
+    }
+    // //back
+    
+
+    public void backtoMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    
+
+    public void OnButtonlevelbackClick()
+    {
+        Application.LoadLevel(19);
+    }
+
+    //busines mode
+    public void OnButtonTCBClick()
+    {
+        Application.LoadLevel(20);
+    }
+    //purchasing mode
+    public void OnButtonTCPClick()
+    {
+        Application.LoadLevel(22);
+    }
+    //office mode
+    public void OnButtonTCOClick()
+    {
+        Application.LoadLevel(21);
+    }
+}
---
+++ Assets/Scenes/Scenetest/forquiz.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forquiz : MonoBehaviour
+{
+    public Text scoreansText;
+    public Text questionText;
+    public Text buttonText1;
+    public Text buttonText2;
+    public Text buttonText3;
+    public Text buttonText4;
+
+    public string question = "";
+    public string answer1 = "";
+    public string answer2 = "";
+    public string answer3 = "";
+    public string answer4 = "";
+
+    public int correctAnswer = -1;
+    static public int scoreans = 0;
+
+    void Start()
+    {
+        questionText.text = question;
+        buttonText1.text = answer1;
+        buttonText2.text = answer2;
+        buttonText3.text = answer3;
+        buttonText4.text = answer4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreansText.text = "score : "+scoreans;
+        
+    }
+    private void setscore()
+    {
+        if((question == "Abide by")||(question == "Considerably")||(question == "Allocate")||(question == "Durable")||(question == "Appliance")||(question == "Convenient")||(question == "Specific")||(question == "Productive")||(question == "Task")||(question == "Collaborate")||(question == "Discount")||(question == "Inquary"))
+                scoreans=0;
+    }
+
+
+    public void OnButton1Click()
+    {
+        if(correctAnswer == 1)
+        {
+            setscore();
+            scoreans++;
+        }   
+        Application.LoadLevel(Application.loadedLevel+1);
+    }
+
+    public void OnButton2Click()
+    {
+        if(correctAnswer == 2)
+        {
+            setscore();
+            scoreans++;
+        }
+        Application.LoadLevel(Application.loadedLevel+1);
+    }
+
+    public void OnButton3Click()
+    {
+        if(correctAnswer == 3)
+        {
+            setscore();
+            scoreans++;
+        }
+        Application.LoadLevel(Application.loadedLevel+1);
+    }
+
+    public void OnButton4Click()
+    {
+        if(correctAnswer == 4)
+        {
+            setscore();
+            scoreans++;
+        }
+        Application.LoadLevel(Application.loadedLevel+1);
+    }
+
+}
